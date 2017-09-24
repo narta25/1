bd7750995ec04206f7bc56291afeb6fb3c1f49de
narta25 <31902575+narta25@users.noreply.github.com>
2017-09-24 19:17:12 +0300

    Add files via upload

---
+++ _4.ino
@@ -0,0 +1,54 @@
+const int greenLEDin = 9;
+const int redLEDin = 11;
+const int blueLEDin = 10;
+
+const int redSensorPin = A0;
+//фоторезистор із червоним фільтром
+const int greenSensorPin = A1;
+//фоторезистор із зеленим фільтром
+const int blueSensorPin = A2;
+//фоторезистор із синім фільтром
+int redValue = 0;
+int greenValue = 0;
+int blueValue = 0;
+
+int redSensorValue = 0;
+int greenSensorValue = 0;
+int blueSensorValue = 0;
+
+void setup() {
+ Serial.begin(9600);
+
+ pinMode(greenLEDin,OUTPUT);
+ pinMode(redLEDin,OUTPUT);
+ pinMode(blueLEDin,OUTPUT);
+}
+
+void loop() {
+  redSensorValue = analogRead(redSensorPin);
+  delay(5);
+  greenSensorValue = analogRead(greenSensorPin);
+  delay(5);
+  blueSensorValue = analogRead(blueSensorPin);
+
+  Serial.print("Raw Sensor Values \t Red: ");
+  Serial.print(redSensorValue);
+  Serial.print(" \t Green: ");
+  Serial.print(greenSensorValue);
+  Serial.print("Raw Sensor Values \t Blue: ");
+  Serial.print(blueSensorValue);
+  redValue = redSensorValue/4;
+  greenValue = greenSensorValue/4;
+  blueValue = blueSensorValue/4;
+
+  Serial.print("Mapped Sensor \t Red: ");
+  Serial.print(redValue);
+  Serial.print(" \t Green: ");
+  Serial.print(greenValue);
+  Serial.print(" \t Blue: ");
+  Serial.println(blueValue);
+
+  analogWrite(redLEDin, redValue);
+  analogWrite(greenLEDin, greenValue);
+  analogWrite(blueLEDin, blueValue);
+}
---
+++ lab3-4.ino
@@ -0,0 +1,53 @@
+const int sensorPin = A0;
+const float baselineTemp = 25.0;//початкова температура
+void setup(){
+  Serial.begin(9600); // open a serial port
+ for(int pinNumber = 2; pinNumber<5; pinNumber++){
+   pinMode(pinNumber,OUTPUT);
+   digitalWrite(pinNumber, LOW);
+  }
+} 
+  void loop(){
+    int sensorVal = analogRead(sensorPin);
+    Serial.print("Sensor Value: ");
+    Serial.print(sensorVal);
+    // convert the ADC reading to voltage
+    float voltage = (sensorVal/1024.0) * 5.0;
+    Serial.print(", Volts: ");
+    Serial.print(voltage);
+    Serial.print(",degrees C: ");
+    //перетворення напруги в температуру в грауси
+    float temperature = (voltage - .5) * 100;
+    Serial.println(temperature);
+    if(temperature < baselineTemp){
+    //температура<заданої температури,всі три 
+    //світлодіода не горять 
+      digitalWrite(2, LOW);//жовтий
+      digitalWrite(3, LOW);//зелений
+      digitalWrite(4, LOW);//червоний
+    }else if (temperature >= baselineTemp+2 &&
+    temperature < baselineTemp+4){
+    //температура >= заданої температури на 2-4 градуси(до 4) 
+    //горить жовтий світлодіод 
+      digitalWrite(2, HIGH);
+      digitalWrite(3, LOW);
+      digitalWrite(4, LOW);
+    }else if (temperature >= baselineTemp+4 &&
+    temperature < baselineTemp+6){
+    //температура >= заданої температури на 4-6 градуси(до 6) 
+    //горить жовтий і зелений світлодіод 
+      digitalWrite(2, HIGH);
+      digitalWrite(3, HIGH);
+      digitalWrite(4, LOW);  
+    }else if (temperature >= baselineTemp+6){
+    //температура >= заданої температури на 6 і більше градусів 
+    //горить жовтий,зелений,червоний світлодіод  
+      digitalWrite(2, HIGH);
+      digitalWrite(3, HIGH);
+      digitalWrite(4, HIGH);  
+    }
+    delay(10);
+    //затримка 10 мс
+  }
+
+
